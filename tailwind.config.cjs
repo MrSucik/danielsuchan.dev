75be9c87cd1109a015d284b87fe5ab1de06d4af5
Daniel Suchan <mr.sucik@gmail.com>
2022-10-23 19:28:23 +0200

    feat: resume styling WIP

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "dark-bg": "#191919",
+    },
+    extend: {
+      backgroundImage: {
+        header: "url('/assets/header-bg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
